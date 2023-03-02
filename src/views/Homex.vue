d59f99d7466e8473663589d8ebcb0ef2ef2fbd36
Emre Kaygusuz <emrekaygusuz95@gmail.com>
2023-03-02 21:04:33 +0100

    none

--- src/views/Homex.vue
+++ src/views/Homex.vue
@@ -158,13 +158,12 @@
         <!-- FOOTER -->
         <div class="footer p-5 d-flex flex-column container text-center">
             <div class="footer-title d-flex flex-column">
-                <h2 class="fw-bold" style="color:#ff2c00;">AUS DEM <span class="px-2"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                            class="bi bi-chat-square-heart" viewBox="0 0 16 16">
+                <h2 class="fw-bold" style="color:#ff2c00;">WE ARE <span class="px-2"><svg xmlns="http://www.w3.org/2000/svg"
+                            width="32" height="32" fill="currentColor" class="bi bi-chat-square-heart" viewBox="0 0 16 16">
                             <path
                                 d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12ZM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Z" />
                             <path d="M8 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z" />
-                        </svg></span> DES KÖLNER AGNESVIERTELS</h2>
+                        </svg></span> DESIGN FOR YOU</h2>
                 <h6 class="text-light">- THE EVK TEAM -</h6>
             </div>
             <ul class="footer-icons text-light d-flex justify-content-center">
